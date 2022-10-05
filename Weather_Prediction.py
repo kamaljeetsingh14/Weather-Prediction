ec2fe0f5cb52ed213d8882be5d63298002fc8682
kamaljeetsingh14 <113400443+kamaljeetsingh14@users.noreply.github.com>
2022-10-04 23:03:26 -0700

    Add files via upload

---
+++ Weather_Prediction.py
@@ -0,0 +1,80 @@
+import pandas as pd
+global_temp = pd.read_csv("GlobalTemperatures.csv")
+print(global_temp.shape)
+print(global_temp.columns)
+print(global_temp.info())
+print(global_temp.isnull().sum())
+
+#Data Preparation
+def wrangle(df):
+    df = df.copy()
+    df = df.drop(columns=["LandAverageTemperatureUncertainty", "LandMaxTemperatureUncertainty",
+                          "LandMinTemperatureUncertainty", "LandAndOceanAverageTemperatureUncertainty"], axis=1)
+						  
+
+
+def converttemp(x):
+        x = (x * 1.8) + 32
+        return float(x)
+    df["LandAverageTemperature"] = df["LandAverageTemperature"].apply(converttemp)
+    df["LandMaxTemperature"] = df["LandMaxTemperature"].apply(converttemp)
+    df["LandMinTemperature"] = df["LandMinTemperature"].apply(converttemp)
+    df["LandAndOceanAverageTemperature"] = df["LandAndOceanAverageTemperature"].apply(converttemp)
+    df["dt"] = pd.to_datetime(df["dt"])
+    df["Month"] = df["dt"].dt.month
+    df["Year"] = df["dt"].dt.year
+    df = df.drop("dt", axis=1)
+    df = df.drop("Month", axis=1)
+    df = df[df.Year &gt;= 1850]
+    df = df.set_index(["Year"])
+    df = df.dropna()
+    return df
+global_temp = wrangle(global_temp)
+print(global_temp.head())
+
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+corrMatrix = global_temp.corr()
+sns.heatmap(corrMatrix, annot=True)
+plt.show()
+
+
+target = "LandAndOceanAverageTemperature"
+y = global_temp[target]
+x = global_temp[["LandAverageTemperature", "LandMaxTemperature", "LandMinTemperature"]]
+
+
+from sklearn.model_selection import train_test_split
+xtrain, xval, ytrain, yval = train_test_split(x, y, test_size=0.25, random_state=42)
+print(xtrain.shape)
+print(xval.shape)
+print(ytrain.shape)
+print(yval.shape)
+
+
+from sklearn.metrics import mean_squared_error
+ypred = [ytrain.mean()] * len(ytrain)
+print("Baseline MAE: ", round(mean_squared_error(ytrain, ypred), 5))
+
+
+from sklearn.feature_selection import SelectKBest
+from sklearn.ensemble import RandomForestRegressor
+forest = make_pipeline(
+    SelectKBest(k="all"),
+    StandardScaler(),
+    RandomForestRegressor(
+        n_estimators=100,
+        max_depth=50,
+        random_state=77,
+        n_jobs=-1
+    )
+)
+forest.fit(xtrain, ytrain)
+
+
+import numpy as np
+errors = abs(ypred - yval)
+mape = 100 * (errors/ytrain)
+accuracy = 100 - np.mean(mape)
+print("Random Forest Model: ", round(accuracy, 2), "%")
